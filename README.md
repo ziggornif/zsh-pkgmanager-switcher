54a76f9f35616ccd6ff10dd9f986ed5a68d9353a
ziggornif <matthieudrouian@gmail.com>
2024-06-19 18:13:58 +0200

    feat: fnm and nvm support - the plugin now support fnm and nvm to update node version from .nvmrc file - corepack command is now called only once

--- README.md
+++ README.md
@@ -8,11 +8,31 @@ See [INSTALL.md](./INSTALL.md).
 
 ## Usage
 
-The plugin automatically detect package.json of your projects.
+### Node.JS version
+
+The plugin automatically detect package.json and `.nvmrc` on your projects.
+
+If the project contains a `.nvmrc` file, the plugin load the specified version (like https://github.com/Sparragus/zsh-auto-nvm-use).
+
+Nvm and Fnm Node.js version manager are both supported.
+
+If there is no version manager installed, the extension print the current Node.JS installed version.
+
+### Package manager
 
 If the packageManager key is present in the package.json file, the corepack command is executed to load the desired package manager.
 
 The default manager package is npm.
+
+### NVM special case
+
+Declare the `NVM_DIR` variable **before** the plugins config in your `.zshrc` file.
+
+```sh
+export NVM_DIR=...
+
+plugins=(zsh-pkgmanager-switcher)
+```
 
 ## Demo
 
--- zsh-pkgmanager-switcher.zsh
+++ zsh-pkgmanager-switcher.zsh
@@ -1,17 +1,37 @@
-_pkgmanager_switcher() {
-  if [ -f "package.json" ]; then
-    package_json_content=$(cat package.json)
-    package_manager_line=$(echo "$package_json_content" | grep '"packageManager"')
-
-    if [ -n "$package_manager_line" ]; then
-      package_manager_value=$(echo "$package_manager_line" | awk -F: '{gsub(/[",]/, "", $2); print $2}')
-      package_name=$(echo "$package_manager_value" | sed 's/^\([^@]*\).*/\1/' | tr -d ' ')
-      corepack enable
-    else
-      package_name="npm"
+autoload -U load-extension
+function load-nvmrc() {
+  if [ -f ".nvmrc" ]; then
+    if type "nvm" > /dev/null 2>&1; then
+      nvm use > /dev/null 2>&1
+    elif type "fnm" > /dev/null 2>&1; then
+      fnm use > /dev/null 2>&1
     fi
+  fi  
+  echo "Node $(printf '\033[0;32m%s\033[0m' $(node -v))"
+}
 
-    echo "Using Node $(node -v) and $package_name $($package_name -v)"
+function load-pkgmanager() {
+  package_json_content=$(cat package.json)
+  package_manager_line=$(echo "$package_json_content" | grep '"packageManager"')
+
+  if [ -n "$package_manager_line" ]; then
+    package_manager_value=$(echo "$package_manager_line" | awk -F: '{gsub(/[",]/, "", $2); print $2}')
+    package_name=$(echo "$package_manager_value" | sed 's/^\([^@]*\).*/\1/' | tr -d ' ')
+  else
+    package_name="npm"
+  fi
+  echo "📦 $(printf '\033[1;31m%s\033[0m' $package_name\ $($package_name -v))"
+}
+
+load-extension() {
+  if [ -z "$COREPACK_ENABLED" ]; then
+    COREPACK_ENABLED=true
+    corepack enable
+  fi
+
+  if [ -f "package.json" ]; then
+    echo "Using $(load-nvmrc) ($(load-pkgmanager))"
   fi
 }
-add-zsh-hook chpwd _pkgmanager_switcher
+add-zsh-hook chpwd load-extension
+load-extension
